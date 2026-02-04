a7589a9b2e9229e48cc964eb700783d1f89ec4d7
Bryna Hazelton <bryna.hazelton@gmail.com>
2026-02-03 23:15:47 -0800

    Add code to read in foliage density data

--- src/bird_diversity/data_io.py
+++ src/bird_diversity/data_io.py
@@ -3,6 +3,7 @@ Read in the data.
 """
 import pathlib
 
+import numpy as np
 import pandas as pd
 
 
@@ -32,3 +33,41 @@ def read_bird_counts(file: str | pathlib.Path) -> pd.DataFrame:
 
 
     return bird_df
+
+
+def read_foliage_density(file: str | pathlib.Path) -> pd.DataFrame:
+    """
+    Read in the foliage density file and returns a dataframe with probabilities.
+    
+    Parameters
+    ----------
+    file : file, str, pathlib.Path, list of str, generator
+        File containing bird counts to read in.
+    
+    Returns:
+    --------
+    foliage_df : pd.DataFrame
+        A dataframe with foliage density probabilities. Columns are sites, rows
+        are layers. Columns should sum to 1.
+    
+    """
+    density_df = pd.read_csv(file, header=0, index_col=0)
+
+    # setup df for total foliage, computed as a trapezoidal area:
+    #  (height change) * (sum of densities at top & bottom) / 2
+    total_df = pd.DataFrame(0, index=density_df.index, columns=density_df.columns) 
+
+    # add a zero row for area calc
+    density_df.loc[0] = [0] * len(density_df.columns)
+    density_df.sort_index()
+    
+    height_range = np.asarray(density_df.index[1:] - density_df.index[:-1])
+    for site in density_df:
+        density_sum = density_df[site][:-1].values + density_df[site][1:].values
+        total_df[site] = (height_range * density_sum)/2
+
+    # convert to probability
+    for site in total_df:
+        total_df[site] = total_df[site] / total_df[site].sum()
+
+    return total_df
